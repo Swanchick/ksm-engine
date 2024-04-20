8c50d356b6d613af5dedc41ca897df996c9b845b
Swanchick <61909965+Swanchick@users.noreply.github.com>
2024-04-20 19:38:28 +0300

    Added new permissions

--- permission/permissions.py
+++ permission/permissions.py
@@ -7,4 +7,8 @@ class Permissions(Enum):
     INSTANCE_START_STOP = 4
     INSTANCE_SHOW_STATS = 8
     INSTANCE_USER_EDIT = 16
+    FILES_SHOW = 32
+    FILES_EDIT = 64
+    FILES_VIEW = 128
+    FILES_CREATE = 256
     
