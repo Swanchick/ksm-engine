cba45e7390842e1f8cfba063375644469d47024a
Swanchick <61909965+Swanchick@users.noreply.github.com>
2024-05-01 19:22:19 +0300

    Fixed bugs

--- server/instance_loader.py
+++ server/instance_loader.py
@@ -2,28 +2,33 @@ from utils import RepoDownloader
 from settings import SettingsCreator
 
 
-INSTANCE_LOADER = "instance_packages"
+INSTANCE_PACKAGES = "instance_packages"
 
 
 class InstanceLoader:
     __name: str
+    __instance_type: str
     __repo_url: str
     __instance_folder: str
 
     def __init__(self, instance_folder: str, name: str, instance_type: str):
         self.__name = name
+        self.__instance_type = instance_type
         self.__instance_folder = instance_folder
 
+    def start(self) -> bool:
         settings_creator = SettingsCreator()
         data = settings_creator.data()
-        if INSTANCE_LOADER not in data:
-            return
+        if INSTANCE_PACKAGES not in data:
+            return False
 
-        data = data[INSTANCE_LOADER]
-        if instance_type not in data:
-            return
+        instance_packages = data[INSTANCE_PACKAGES]
+        if self.__instance_type not in instance_packages:
+            return False
 
-        self.__repo_url = data[instance_type]
+        self.__repo_url = instance_packages[self.__instance_type]
+
+        return True
 
     def load(self):
         path = f"{self.__instance_folder}{self.__name}/"
